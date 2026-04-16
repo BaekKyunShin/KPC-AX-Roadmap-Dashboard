0b32e791e3cb66049065e80fccb2136163e728b0
신백균 <werooring@gmail.com>
2026-04-17 00:14:50 +0900

    feat(ofa-shared): 공용 디자인 키트 배럴 export 추가

---
+++ src/components/roadmap/shared/index.ts
@@ -0,0 +1,19 @@
+/**
+ * 로드맵 표 공용 디자인 키트 배럴 export.
+ *
+ * 사용 예:
+ *   import { TableTextCell, SyncedTableRow } from '@/components/roadmap/shared';
+ */
+
+export {
+  TABLE_CELL_TEXT_CLASS,
+  TABLE_CELL_INLINE_CLASS,
+  READ_ONLY_TEXT_CLASS,
+  CARD_HEADER_CLASS,
+} from './table-styles';
+
+export { TableTextCell, type TableTextCellProps } from './TableTextCell';
+export { TableInlineCell, type TableInlineCellProps } from './TableInlineCell';
+export { TableNumericCell, type TableNumericCellProps } from './TableNumericCell';
+export { SyncedTableRow, type SyncedTableRowProps } from './SyncedTableRow';
+export { SectionNumberBadge, type SectionNumberBadgeProps } from './SectionNumberBadge';
